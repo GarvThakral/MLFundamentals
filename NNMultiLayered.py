89f6542f006a8f7841dc40af413c1629ae9187a6
Garv Thakral <garvthakralfx@gmail.com>
2025-06-17 14:08:11 +0530

    NN for L layers

--- NNMultiLayered.py
+++ NNMultiLayered.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def sigmoid(z):
     result = 1/(1+np.exp(-z))
-    return result
+    return result, z
 
 def relu(z):
-    result = max(0,z)
-    return result
+    result = np.maximum(0,z)
+    return result,z
 
 def initialize_weights(dimensions):
     parameters = {}
@@ -17,25 +17,92 @@ def initialize_weights(dimensions):
 
     return parameters
 
-def linear_forward(X,A_prev,W,b):
-    Z = np.dot(W,X) + b
-    linear_cache = {
-        "A_prev":A_prev,
-        "W":W,
-        "b":b
-    }
-    return Z , linear_cache
+def linear_forward(A,W,b):
+    Z = np.dot(W,A) + b
+    cache = (A, W, b)
+    
+    return Z , cache
 
-def linear_activation_forward(X,Y,A_prev,activation):
+def linear_activation_forward(A_prev,W,b,activation):
+    linear_cache = None
+    activation_cache = None
+    A = None
     if activation == "relu":
-        Z,cache = 
-        A = 
-        pass
+        Z , linear_cache = linear_forward(A_prev,W,b)
+        A , activation_cache = relu(Z) 
+        
     elif activation == "sigmoid":
-        pass
-    return A , activation_cache
+        Z , linear_cache = linear_forward(A_prev,W,b)
+        A , activation_cache = sigmoid(Z)
+    
+    cache = (linear_cache,activation_cache)
+    return A , cache
 
+def L_model_forward(X,parameters):
+    caches = []
+    A = X
+    L = len(parameters) // 2 
+    for l in range(1,L):
+        A_prev = A
+        A,cache = linear_activation_forward(A_prev,parameters["W"+str(l)],parameters["b"+str(l)],activation = "relu")
+        caches.append(cache)
+    AL,cache = linear_activation_forward(A,parameters["W"+str(L)],parameters["b"+str(L)],activation = "sigmoid")
+    caches.append(cache)
+    return AL,caches
+
+
+def update_parameters(paramters,grads):
+    pass
+        
+
+def compute_cost(AL,Y):
+    m = Y.shape[1]
+    cost  = -1/m*np.sum((Y*np.log(AL)+(1-Y)*np.log(1-AL)))
+    return cost
+
+def linear_activation_backward(dA , cache ,activation):
+    linear_cache , activation_cache = cache 
+    if activation == "sigmoid":
+        dZ = sigmoid_backward(dA,activation_cache)    
+        dA_prev,dW,db = linear_backward(dZ,cache)
+        return dA_prev , dW , db
+    elif activation == "relu":
+        dZ = relu_backward(dA,activation_cache)   
+        dA_prev,dW,db = linear_backward(dZ,cache) 
+        return dA_prev , dW , db
+
+def linear_backward(dZ, cache):
+    linear_cache, activation_cache = cache
+    A_prev, W, b = linear_cache
+    m = A_prev.shape[1]
+    dW = 1/m*np.dot(dZ,A_prev.T)
+    db = 1/m*np.sum(dZ,axis = 1,keepdims = True)
+    dA_prev = np.dot(W.T,dZ)
+    
+    return dA_prev,dW,db
+
+
+def relu_backward(dA,activation_cache):
+    Z = activation_cache
+    dZ = dA * (Z > 0)
+    return dZ
+
+def sigmoid_backward(dA,activation_cache):
+    Z = activation_cache 
+    A = 1 / (1 + np.exp(-Z)) 
+    dZ = dA * A * (1 - A)
+    return dZ
+
+def L_model_backward(AL,Y,caches):
+    grads = {}
+    L = len(caches) # the number of layers
+    m = AL.shape[1]
+    Y = Y.reshape(AL.shape)
+    dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+    for l in reversed(range(0,L-1)):
+        dA_prev , dW , db = linear_activation_forward(A_prev,W,b,activation) 
 
 dimensions = [3,4,1]
+
 parameters = initialize_weights(dimensions)
 print(parameters)
